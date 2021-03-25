e131fd17cc52acdb0c6940b4a98628d32e8364f6
natdoan <48767986+natdoan@users.noreply.github.com>
2021-03-25 10:46:51 -0700

    Update app.py

--- processing/app.py
+++ processing/app.py
@@ -34,6 +34,16 @@ else:
 
 with open(app_conf_file, 'r') as f:
     app_config = yaml.safe_load(f.read())
+    
+# External Logging Configuration
+with open(log_conf_file, 'r') as f:
+    log_config = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_config)
+    
+logger = logging.getLogger('basicLogger')
+
+logger.info("App Conf File: %s" % app_conf_file)
+logger.info("Log Conf File: %s" % log_conf_file)
 
 def populate_stats():
 
