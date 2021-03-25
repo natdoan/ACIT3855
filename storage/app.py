a820e17c9b7eea6d2ce0440ad3e34f65646fd3a1
natdoan <48767986+natdoan@users.noreply.github.com>
2021-03-25 10:47:29 -0700

    Update app.py

--- storage/app.py
+++ storage/app.py
@@ -41,6 +41,16 @@ else:
 
 with open(app_conf_file, 'r') as f:
     app_config = yaml.safe_load(f.read())
+    
+# External Logging Configuration
+with open(log_conf_file, 'r') as f:
+    log_config = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_config)
+    
+logger = logging.getLogger('basicLogger')
+
+logger.info("App Conf File: %s" % app_conf_file)
+logger.info("Log Conf File: %s" % log_conf_file)
 
 DB_ENGINE = create_engine("mysql+pymysql://" + app_config["datastore"]["user"] + ":" + app_config["datastore"]["password"] + "@" + app_config["datastore"]["hostname"] + ":" + str(app_config["datastore"]["port"]) + "/" + app_config["datastore"]["db"])
 Base.metadata.bind = DB_ENGINE
