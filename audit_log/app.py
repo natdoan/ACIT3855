3b70d62e272c432dc7e3a72689d282e3870ec6db
natdoan <48767986+natdoan@users.noreply.github.com>
2021-03-25 09:35:43 -0700

    Update app.py
    loads external conf files

--- audit_log/app.py
+++ audit_log/app.py
@@ -12,16 +12,39 @@ from pykafka import KafkaClient
 #from pykafka.common import OffsetType
 #from threading import Thread
 
-with open('app_conf.yaml', 'r') as f:
-    app_config = yaml.safe_load(f.read())
+#with open('app_conf.yaml', 'r') as f:
+#    app_config = yaml.safe_load(f.read())
 
-with open('log_conf.yaml', 'r') as f:
+#with open('log_conf.yaml', 'r') as f:
+#    log_config = yaml.safe_load(f.read())
+#    logging.config.dictConfig(log_config)
+
+#logger = logging.getLogger('basicLogger')
+
+import os
+
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yaml"
+    log_conf_file = "/config/log_conf.yaml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yaml"
+    log_conf_file = "log_conf.yaml"
+
+with open(app_conf_file, 'r') as f:
+    app_config = yaml.safe_load(f.read())
+    
+# External Logging Configuration
+with open(log_conf_file, 'r') as f:
     log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
-
+    
 logger = logging.getLogger('basicLogger')
 
-
+logger.info("App Conf File: %s" % app_conf_file)
+logger.info("Log Conf File: %s" % log_conf_file)
+    
 def get_calorie_intake_report(index):
 
     hostname = "%s:%d" % (app_config["events"]["hostname"],
