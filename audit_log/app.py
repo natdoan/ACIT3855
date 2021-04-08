4b9ad54df45bab669a6d0e01a71c5fa6bfca802e
natdoan <48767986+natdoan@users.noreply.github.com>
2021-04-08 10:16:45 -0700

    Update app.py

--- audit_log/app.py
+++ audit_log/app.py
@@ -118,8 +118,9 @@ def get_weight_report(index):
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-CORS(app.app)
-app.app.config['CORS_HEADERS'] = 'Content-Type'
+if "TARGET_ENV" not in os.environ or os.environ["TARGET_ENV"] != "test":
+      CORS(app.app)
+      app.app.config['CORS_HEADERS'] = 'Content-Type'
 
 #app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
 app.add_api("openapi.yaml", base_path="/audit_log", strict_validation=True, validate_responses=True)
