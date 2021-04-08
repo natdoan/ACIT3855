74f50a408a2fb3508b2cd49d83a70dadba428ed0
natdoan <48767986+natdoan@users.noreply.github.com>
2021-04-07 21:37:06 -0700

    Update app.py

--- receiver/app.py
+++ receiver/app.py
@@ -43,15 +43,29 @@ logger = logging.getLogger('basicLogger')
 logger.info("App Conf File: %s" % app_conf_file)
 logger.info("Log Conf File: %s" % log_conf_file)
 
+hostname = "%s:%d" % (app_config["events"]["hostname"], app_config["events"]["port"])
+retry_count = 0
+
+while retry_count < app_config["events"]["max_try"]:
+    logger.info("Connecting to Kafka.")
+    try:
+        client = KafkaClient(hosts=hostname)
+        topic = client.topics[str.encode(app_config["events"]["topic"])]
+        logger.info("Connected to Kafka!")
+    except:
+        retry_count = retry_count + 1
+        logger.error("Connection failed. Retrying in 5 seconds.")
+        time.sleep(app_config["events"]["sleep"])
+
 def report_calorie_intake(body):
 
     logger.debug(("DEBUG: " + str(body)))
     logger.info("INFO: Received event Calorie Intake request with a unique id of " + str(body['client_id']))
 
-    hostname = "%s:%d" % (app_config["events"]["hostname"], app_config["events"]["port"])
-    client = KafkaClient(hosts=hostname)
-    topic_name = app_config["events"]["topic"]
-    topic = client.topics[str.encode(topic_name)]
+    #hostname = "%s:%d" % (app_config["events"]["hostname"], app_config["events"]["port"])
+    #client = KafkaClient(hosts=hostname)
+    #topic_name = app_config["events"]["topic"]
+    #topic = client.topics[str.encode(topic_name)]
 
     producer = topic.get_sync_producer()
     logger.info("this inst working")
@@ -74,10 +88,10 @@ def report_weight(body):
     logger.debug(("DEGBUG: " + str(body)))
     logger.info("INFO: Received event Weight request with a unique id of " + str(body['client_id']))
 
-    hostname = "%s:%d" % (app_config["events"]["hostname"], app_config["events"]["port"])
-    client = KafkaClient(hosts=hostname)
-    topic_name = app_config["events"]["topic"]
-    topic = client.topics[str.encode(topic_name)]
+    #hostname = "%s:%d" % (app_config["events"]["hostname"], app_config["events"]["port"])
+    #client = KafkaClient(hosts=hostname)
+    #topic_name = app_config["events"]["topic"]
+    #topic = client.topics[str.encode(topic_name)]
     producer = topic.get_sync_producer()
 
     msg = {"type": "w",
