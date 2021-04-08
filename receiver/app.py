e5b715f956a609055eabd23d98f46b5bcfea0c6c
natdoan <48767986+natdoan@users.noreply.github.com>
2021-04-08 10:12:16 -0700

    Update app.py

--- receiver/app.py
+++ receiver/app.py
@@ -112,7 +112,8 @@ def report_weight(body):
 
 app = connexion.FlaskApp(__name__, specification_dir='')
 
-app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
+#app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
+app.add_api("openapi.yaml", base_path="/receiver", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
     app.run(port=8080)
